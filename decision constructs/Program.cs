15b4fea5a46a48bbc885cfb432449d42465cc2e2
George Lebury <gledbury4@gmail.com>
2019-10-09 15:41:55 -0700

    iteration/decision constructs wip

---
+++ decision constructs/Program.cs
@@ -0,0 +1,58 @@
+﻿/*
+Author: George Ledbury
+Date: Oct. 09, 2016
+CTEC 135: Microsoft Software Development with C#
+
+<Module # 2, Programming Assignment # 1 Part 2, Problem # 3: Decision Construct
+
+
+Region 1: print the digits 1-5 using a for loop.
+Region 2: print the digits 1-5 using a while loop.
+Region 3: print the digits 1-5 using a do/while loop.
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace decision_constructs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for ()
+            {
+                for ()
+                {
+                    for ()
+                    {
+
+                    }
+                }
+            }
+        }
+
+        static void powerCable()
+        {
+            Console.WriteLine("Check the Power Cord.");
+        }
+        static void printerCable()
+        {
+            Console.WriteLine("Check the Printer Cable.");
+        }
+        static void printerSoftware()
+        {
+            Console.WriteLine("Check that Printer Software is Installed Correctly");
+        }
+        static void ink()
+        {
+            Console.WriteLine("Check/Replace Ink.");
+        }
+        static void paper()
+        {
+            Console.WriteLine("check for Paper Jam");
+        }
+    }
+}
---
+++ iterationConstructs/Program.cs
@@ -0,0 +1,63 @@
+﻿/*
+Author: George Ledbury
+Date: Oct. 09, 2016
+CTEC 135: Microsoft Software Development with C#
+
+<Module # 2, Programming Assignment # 1 Part 2, Problem # 2: Iteration Constructs>
+
+Region 1: print the digits 1-5 using a for loop.
+Region 2: print the digits 1-5 using a while loop.
+Region 3: print the digits 1-5 using a do/while loop.
+*/
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iterationConstructs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region print the numbers 1-5 with a for loop structures
+            Console.WriteLine("print the numbers 1-5 using a for loop with a space betweeneach number.");
+            Console.WriteLine();
+            for (int i = 1; i < 6; i++)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            #endregion
+
+            #region print the numbers 1-5 using a while loop
+            Console.WriteLine("print the numbers 1-5 using a while loop with a space between each number.");
+            Console.WriteLine();
+            int count = 1;
+            while (count < 6)
+            {
+                Console.Write(count + " ");
+                count++;
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            #endregion
+
+            #region print the numbers 1-5 using a do/while loop
+            Console.WriteLine("print the numbers 1-5 using a do/while loop with a space between each number.");
+            Console.WriteLine();
+            int number = 1;
+            do
+            {
+                Console.Write(number + " ");
+                number++;
+            } while (number < 6);
+            Console.WriteLine();
+            #endregion
+        }
+    }
+}
